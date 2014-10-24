db6a9a5d520a001b092414cdcf6a6916ecf7989b
okabi <okabi13@yahoo.co.jp>
2014-10-24 13:58:59 +0900

    MeCab(辞書以外)追加

---
+++ MeCabMorphologicalAnalyzer/ConsoleDebug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeCabMorphologicalAnalyzer
+{
+    class ConsoleDebug
+    {
+        static void Main(string[] args)
+        {
+            // 形態素解析のためのインスタンス作成
+            MeCabMorphologicalAnalyzer mcma = new MeCabMorphologicalAnalyzer();
+
+            // 形態素解析する文字列の入力
+            string str = Console.ReadLine();
+
+            // 形態素解析の実行
+            List<Morpheme> morphemes = mcma.Analyse(str);
+
+            // 結果の表示
+            foreach (Morpheme elem in morphemes)
+            {
+                Console.WriteLine(elem.ToString());
+            }
+
+            // Enterで終了
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MeCabMorphologicalAnalyzer/Morpheme.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeCabMorphologicalAnalyzer
+{
+    /// <summary>形態素を制御するクラス。</summary>
+    public class Morpheme
+    {
+        /// <summary>語の表記</summary>
+        public string Surface { get; private set; }
+        /// <summary>語の品詞</summary>
+        public string Pos { get; private set; }
+
+        /// <summary>
+        /// 形態素情報を渡してインスタンスを生成する。
+        /// </summary>
+        /// <param name="surface">語の表記</param>
+        /// <param name="pos">語の品詞</param>
+        public Morpheme(string surface, string pos)
+        {
+            Surface = surface;
+            Pos = pos;
+        }
+
+        /// <summary>
+        /// 表記(品詞) 形式の文字列を返す。
+        /// </summary>
+        /// <returns>表記(品詞) 形式の文字列</returns>
+        public override string ToString()
+        {
+            return Surface + "(" + Pos + ")";
+        }
+    }
+}
